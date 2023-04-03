0d633406d32a4c82e654ac940dfdecc910815e58
kundan kumar <129506620+kundankumar95@users.noreply.github.com>
2023-04-03 07:28:07 +0530

    Add files via upload

---
+++ REGISTRATION RECORD/PROJECT.C
@@ -0,0 +1,375 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+
+void add();  //FUNCTIONS
+void list();
+void edit();  //GLOBALLY DECLARED FUNCTIONS N VARIABLE
+void delete1();
+void search();
+
+void setcolor(int ForgC)
+{ WORD wColor;
+HANDLE hStdOut=GetStdHandle(STD_OUTPUT_HANDLE);
+CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+if(GetConsoleScreenBufferInfo(hStdOut,&csbi))
+{
+	wColor=(csbi.wAttributes & 0xB0)+(ForgC & 0x0B);
+//	SetConsoleTextAttributes(hStdOut,wColor);
+	SetConsoleTextAttribute(hStdOut,wColor);
+
+}
+}
+
+void login()
+{
+
+	int a=0,i=0;
+    char uname[10],c=' ';
+    char pword[10],code[10];
+    char user[10]="user";
+    char pass[10]="pass";
+    do
+{
+	system("cls");
+
+    printf("\n  *********  LOGIN FORM  *********  ");
+    printf(" \n                       ENTER USERNAME:-");
+	scanf("%s", &uname);
+	printf(" \n                       ENTER PASSWORD:-");
+	while(i<10)
+	{
+	    pword[i]=getch();
+	    c=pword[i];
+	    if(c==13) break;
+	    else printf("*");
+	    i++;
+	}
+	pword[i]='\0';
+	//char code=pword;
+	i=0;
+	//scanf("%s",&pword);
+		if(strcmp(uname,user)==0 && strcmp(pword,pass)==0)
+	{
+      printf("\n        WELCOME!  LOGIN IS SUCESSFUL");
+	
+
+	break;
+	}
+	else
+	{
+      printf("\n        SORRY !!!!  LOGIN IS UNSUCESSFUL");
+		
+		a++;
+
+		getch();
+
+	}
+}
+	while(a<=2);
+	if (a>2)
+	{
+		printf("\nSorry you have entered the wrong username and password for four times!!!");
+
+		getch();
+
+		}
+		system("cls");
+}
+
+struct CustomerDetails   //STRUCTURE DECLARATION
+{
+	char vehiclenumber[10];
+	char name[20];
+	char address[25];
+	char phonenumber[35];
+	char nationality[15];
+	char email[20];
+	char amount[10];
+	char date[10];
+}s;
+
+int main(){     // MAIN FUNCTION
+	int i=0;
+
+	time_t t;
+	time(&t);
+	char customername;
+	char choice;
+    login();
+    system("cls");
+	while (1)      // INFINITE LOOP
+	{
+		system("cls");
+		setcolor(10);
+		 for(i=0;i<80;i++)
+		printf("-");
+		printf("\n");
+		printf("   **********  |MAIN MENU|  *********** \n");
+		for(i=0;i<80;i++)
+		printf("-");
+		printf("\n");
+		setcolor(10);
+		printf("\t\t Please enter your choice for menu:");
+		printf("\n\n");
+		printf(" \n Enter 1 -> Book a vehicle");
+		printf("\n------------------------");
+		printf(" \n Enter 2 -> View Customer Record");
+		printf("\n----------------------------------");
+		printf(" \n Enter 3 -> Delete Customer Record");
+		printf("\n-----------------------------------");
+		printf(" \n Enter 4 -> Search Customer Record");
+		printf("\n-----------------------------------");
+		printf(" \n Enter 5 -> Edit Record");
+		printf("\n-----------------------");
+		printf(" \n Enter 6 -> Exit");
+		printf("\n-----------------");
+		printf("\n");
+		for(i=0;i<80;i++)
+		printf("-");
+	    printf("\nCurrent date and time : %s",ctime(&t));
+	    for(i=0;i<80;i++)
+		printf("-");
+
+		choice=getche();
+		choice=toupper(choice);
+		switch(choice)           // SWITCH STATEMENT
+		{
+			case '1':
+				add();break;
+			case '2':
+				list();break;
+			case '3':
+				delete1();break;
+			case '4':
+				search();break;
+			case '5':
+				edit();break;
+			case '6':
+				system("cls");
+				printf("\n\n\t **THANK YOU**");
+				printf("\n\t FOR TRUSTING OUR SERVICE");
+			//	Sleep(2000);
+				exit(0);
+				break;
+			default:
+				system("cls");
+				printf("Incorrect Input");
+				printf("\n Press any key to continue");
+				getch();
+		}
+	}
+}
+
+void add()
+{
+	FILE *f;
+	char test;
+	f=fopen("add.txt","a+");
+	if(f==0)
+	{   f=fopen("add.txt","w+");
+		system("cls");
+		printf("Please hold on while we set our database in your computer!!");
+		printf("\n Process completed press any key to continue!! ");
+		getch();
+	}
+	while(1)
+	{
+		system("cls");
+		printf("\n Enter Customer Details:");
+		printf("\n**********");
+		printf("\n Enter vehicle number:\n");
+		scanf("\n%s",s.vehiclenumber);
+		fflush(stdin);
+		printf("Enter Name:\n");
+		scanf("%s",s.name);
+		printf("Enter Address:\n");
+		scanf("%s", s .address);
+		printf("Enter Phone Number:\n");
+		scanf("%s", s.phonenumber);
+		printf("Enter Nationality:\n");
+		scanf("%s",  s.nationality);
+		printf("Enter Email:\n");
+		scanf(" %s", s.email);
+		printf("Enter amount:\n");
+		scanf("%s",&s.amount);
+		printf("Enter  date(dd\\mm\\yyyy):\n");
+		scanf("%s",&s.date);
+		fwrite(&s,sizeof(s),1,f);
+		fflush(stdin);
+		printf("\n\n1 vehicle is successfully booked!!");
+		printf("\n Press esc key to exit,  any other key to add another customer detail:");
+		test=getche();
+		if(test==27)
+			break;
+
+	}
+	fclose(f);
+}
+
+void list()
+{
+	FILE *f;
+	int i;
+	if((f=fopen("add.txt","r"))==NULL)
+		exit(0);
+	system("cls");
+	printf("VEHICLE    ");
+	printf("NAME\t ");
+	printf("\tADDRESS ");
+	printf("\tPHONENUMBER ");
+	printf("\tNATIONALITY ");
+	printf("\tEMAIL ");
+	printf("\t\tADVANCE ");
+	printf("\t\tDATE \n");
+
+	for(i=0;i<118;i++)
+		printf("-");
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		printf("VEHICLENUMBER :\t%s\n",s.vehiclenumber);
+		printf("NAME:\t%s\n",s.name);
+		printf("ADDRESS:\t%s\n",s.address);
+		printf("PHONENUMBER:\t%s\n",s.phonenumber);
+		printf("NATIONALITY \n");
+		printf("\n%s \t%s \t\t%s \t%s \t\t%s \t\t%s \t%s \t\t%s",s.vehiclenumber, s.name , s.address , s.phonenumber ,s.nationality ,s.email,   s.amount,  s.date);
+	}
+	printf("\n");
+	for(i=0;i<118;i++)
+		printf("-");
+
+	fclose(f);
+	getch();
+}
+
+void delete1()
+{
+	FILE *f,*t;
+	int i=1;
+	char vehiclenumber[20];
+	if((t=fopen("temp.txt","w"))==NULL)
+	exit(0);
+	if((f=fopen("add.txt","r"))==NULL)
+	exit(0);
+	system("cls");
+	printf("Enter the vehicle Number of the showroom to be deleted from the database: \n");
+	fflush(stdin);
+	scanf("%s",vehiclenumber);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.vehiclenumber,vehiclenumber)==0)
+		{       i=0;
+			continue;
+		}
+		else
+			fwrite(&s,sizeof(s),1,t);
+	}
+	if(i==1)
+	{
+		printf("\n\n Records of Customer in this  Vehicle number is not found!!");
+		//remove("E:/file.txt");
+	   //rename("E:/temp.txt","E:/file.txt");
+		getch();
+		fclose(f);
+		fclose(t);
+		main();
+	}
+	fclose(f);
+	fclose(t);
+	remove("add.txt");
+	rename("temp.txt","add.txt");
+	printf("\n\nThe Customer is successfully removed....");
+	fclose(f);
+	fclose(t);
+	getch();
+}
+
+void search()
+{
+system("cls");
+	FILE *f;
+	char vehiclenumber[20];
+	int flag=1;
+	f=fopen("add.txt","r+");
+	if(f==0)
+		exit(0);
+	fflush(stdin);
+	printf("Enter vehicle number of the customer to search its details: \n");
+	scanf("%s", vehiclenumber);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.vehiclenumber,vehiclenumber)==0){
+			flag=0;
+			printf("\n\tRecord Found\n ");
+			printf("\nRoom Number:\t%s",s.vehiclenumber);
+			printf("\nName:\t%s",s.name);
+			printf("\nAddress:\t%s",s.address);
+			printf("\nPhone number:\t%s",s.phonenumber);
+			printf("\nNationality:\t%s",s.nationality);
+			printf("\nEmail:\t%s",s.email);
+			printf("\nPeriod:\t%s",s.amount);
+			printf("\nArrival date:\t%s",s.date);
+			flag=0;
+			break;
+		}
+	}
+	if(flag==1){
+		printf("\n\tRequested Customer could not be found!");
+	}
+	getch();
+	fclose(f);
+}
+
+void edit()
+{
+	FILE *f;
+	int k=1;
+	char vehiclenumber[20];
+	long int size=sizeof(s);
+	if((f=fopen("add.txt","r+"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Enter vehicle number of the customer to edit:\n\n");
+	scanf("%[^\n]",vehiclenumber);
+	fflush(stdin);
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		if(strcmp(s.vehiclenumber,vehiclenumber)==0)
+		{
+			k=0;
+			printf("\nEnter vehicle Number     :");
+			gets(s.vehiclenumber);
+			printf("\nEnter Name    :");
+			fflush(stdin);
+			scanf("%s",&s.name);
+			printf("\nEnter Address        :");
+			scanf("%s",&s.address);
+			printf("\nEnter Phone number :");
+			scanf("%f",&s.phonenumber);
+			printf("\nEnter Nationality :");
+			scanf("%s",&s.nationality);
+			printf("\nEnter Email :");
+			scanf("%s",&s.email);
+			printf("\nEnter Amount :");
+			scanf("%s",&s.amount);
+			printf("\nEnter  date :");
+			scanf("%s",&s.date);
+			fseek(f,size,SEEK_CUR);  //to go to desired position infile
+			fwrite(&s,sizeof(s),1,f);
+			break;
+		}
+	}
+	if(k==1){
+		printf("\n\nTHE RECORD DOESN'T EXIST!!!!");
+		fclose(f);
+		getch();
+	}else{
+	fclose(f);
+	printf("\n\n\t\tYOUR RECORD IS SUCCESSFULLY EDITED!!!");
+	getch();
+}
+}
